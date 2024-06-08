6b40b46b44073e78a8dde67138752465d18fcb26
Andre Barros <andrebarros7201@gmail.com>
2024-06-08 12:20:05 +0100

    add conditional rendering to todoList

--- src/components/DisplayToDo.jsx
+++ src/components/DisplayToDo.jsx
@@ -3,19 +3,24 @@ import "../styles/DisplayTodos.css";
 export default function DisplayToDo({ todoList }) {
   return (
     <div className="displayTodos">
-      {todoList.map((todo) => (
-        <div className="todo" key={todo.id}>
-          <h2 className="title">{todo.todoData.title}</h2>
-          <p className="description">{todo.todoData.description}</p>
-          <p className="priority">{todo.todoData.priority}</p>
-          <p className="dueDate">{todo.todoData.dueDate}</p>
-          <p className="complete">
-            Complete: {todo.todoData.complete ? "✔️" : "❌"}
-          </p>
-          <button className="update">Update</button>
-          <button className="delete">Delete</button>
-        </div>
-      ))}
+      <h2>ToDO List</h2>
+      {todoList.length > 0 ? (
+        todoList.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h2 className="title">{todo.todoData.title}</h2>
+            <p className="description">{todo.todoData.description}</p>
+            <p className="priority">{todo.todoData.priority}</p>
+            <p className="dueDate">{todo.todoData.dueDate}</p>
+            <p className="complete">
+              Complete: {todo.todoData.complete ? "✔️" : "❌"}
+            </p>
+            <button className="update">Update</button>
+            <button className="delete">Delete</button>
+          </div>
+        ))
+      ) : (
+        <h2>There are no ToDo items</h2>
+      )}
     </div>
   );
 }
